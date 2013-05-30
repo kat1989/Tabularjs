b562969b93bfa215700d62efee468035cb4c250b
RaulMonteroC <raulalejandro.montero@gmail.com>
2013-05-30 18:29:02 -0400

    TableGenerator object skeleton file created.

---
+++ Source/TableGenerator.js
@@ -0,0 +1,20 @@
+/**
+ * Author : Raul Montero
+ * Project : Tabular.js
+ * File : TableGenerator.js
+ */
+
+function TableGenerator(){
+    this.editButton = false;
+    this.addButton = false;
+
+    function generate(arr){
+
+
+    }
+}
+
+var generator = new TableGenerator();
+generator.editButton = true;
+
+console.log(generator);
